1264e68936d62e283871a24dba6457a0fa38d757
karlinbarnes <karlinbarnes3@gmail.com>
2024-08-26 20:54:56 -0500

    First commit of the Snake Game project.

---
+++ main.py
@@ -0,0 +1,54 @@
+# The Snake Game project is an example of using OOP in Python.
+# The game is a simple interactive GUI based game.
+
+
+from turtle import Screen
+from scoreboard import Scoreboard
+from snake import Snake
+import time
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move_snake()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    #if head collides with any segment in the tail:
+        #trigger game_over
+
+
+screen.exitonclick()
+
